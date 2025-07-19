3c1bb0e490e02d013b1c32c040b388083a6ec8e9
Mikiish <mikihsa@gmail.com>
2025-07-19 03:58:20 +0200

    init_script for automatic setup

---
+++ codexinit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "[+] Updating Package..."
+sudo apt update
+sudo apt install npm curl git
+echo "[+] Cloning repo from source..."
+mkdir codex-dev
+cp -r codex codex-dev/
+rm -rf codex
+git clone https://github.com/openai/codex.git
+
+echo "[+] Curling nvm version from github.com..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc  # ou ~/.zshrc selon ton shell
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 22
+nvm use 22
+echo "[+] Installing codex from node."
+npm install -g @openai/codex
+echo "[+] Codex installed, run `codex login` to authenticate your ChatGPT account. Then do the following steps :"
+echo "[+] 1) Create ~/.codex/config.toml file to setup your models and parameters."
+echo "[+] --- Visit [codex/blob/rust-v0.8.0/codex-cli/src/utils/model-info.ts] file for more informations on models availables."
+echo "[+] 2) You can authenticate using API keys for automatic completion using agents such as well... Codex itself. Ah!"
+echo "[+] --- See openai official documentation here : [https://github.com/openai/codex/?tab=readme-ov-file]"
